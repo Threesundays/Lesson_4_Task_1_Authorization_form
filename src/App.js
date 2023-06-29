1d525ca6228fb43e175466cf62b0eb545631a3db
Anton Krat <threesundaysonewerk@gmail.com>
2023-06-29 17:33:44 +0800

    initial project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
-import { Playground } from './component/playground';
-import React from 'react';
+import { useState } from 'react';
+import styles from './app.module.css';
+
+const sendData = (formData) => {
+    console.log(formData);
+};
 
 export const App = () => {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+
+	const onSubmit = (event) => {
+		event.preventDefault();
+        sendData({ email, password });
+	};
+
 	return (
-		<React.StrictMode>
-			<Playground />
-		</React.StrictMode>
+		<div className={styles.app}>
+			<form onSubmit={onSubmit}>
+				<input
+					type="email"
+					name="email"
+					value={email}
+					placeholder="Email"
+					onChange={({ target }) => setEmail(target.value)}
+				/>
+				<input
+					type="password"
+					name="password"
+					value={password}
+					placeholder="Pasword"
+					onChange={({ target }) => setPassword(target.value)}
+				/>
+				<input
+					type="password"
+					name="password"
+					value={password}
+					placeholder="Repeat your password"
+					onChange={({ target }) => setPassword(target.value)}
+				/>
+			<button type="submit">Send</button> 
+			</form>
+		</div>
 	);
 };
